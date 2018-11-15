2ee5f6134e92fd8593ab6358d83c41ba255440aa
Jason Sigler <jsigler2@asu.edu>
2018-11-15 00:48:26 -0500

    Update lottosim.py

--- lottosim.py
+++ lottosim.py
@@ -1,22 +1,22 @@
-#A simple exercise in the futility of the lotter
+#A simple exercise in the futility of matching sets of random numbers (much like the lottery). 
 import random
 
 
-tickets = 10000000
-wins = 0
-winnum = random.sample(range(0, 75), 5)
+tickets = 10000000  #The number of times to play
+wins = 0            #The number of games you've won
+winnum = random.sample(range(0, 75), 5)  #Generate your winning array of numbers
 print "The winning number is: %s" % winnum
 
 print "Matching %s against %s tickets......." % (winnum, tickets)
 
 while tickets > 0:
-    ticket = random.sample(range(0, 75), 5)
-    if ticket == winnum:
-        wins += 1
+    ticket = random.sample(range(0, 75), 5) #Creates a ticket number
+    if ticket == winnum: #Matches ticket against winning number
+        wins += 1 #
         tickets -= 1
     else:
         tickets -= 1
     
 
 
-print "There were %s matches" % wins
+print "There were %s matches" % wins   #Report the number of wins
